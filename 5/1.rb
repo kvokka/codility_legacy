87b6782cc843dd988b119c954dc8793fe5ff7bb1
kvokka <root_p@mail.ru>
2015-12-27 10:48:17 +0100

    5-1, 5-2

---
+++ 5/1.rb
@@ -0,0 +1,5 @@
+def solution(a, b, k)
+    # write your code in Ruby 2.2
+    offset = a % k
+    offset==0 ? ((b-a) / k)+1 : (offset+b-a)/k
+end
---
+++ 5/2.rb
@@ -0,0 +1,15 @@
+def solution(a)
+    # write your code in Ruby 2.2
+    e=w=res=0
+    a.each {|i| i==0 ? e+=1 : w+=1 }
+    a.each do |i|
+       if i==0
+          res+=w
+          e-=1
+       else
+          w-=1
+       end
+       return -1 if res> 1_000_000_000
+       return res if e<=0 || w<=0
+    end
+end
